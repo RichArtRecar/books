50d2a9cc89a5290aa93ec45ab90c7e09bd2eaa78
Rich Recar <richard_recar@outlook.com>
2019-05-29 16:21:15 -0400

    "Npm script"

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 //List of requires
 var express = require('express');
 var chalk = require('chalk');
+
 var debug = require('debug')('app');
+
+
 var morgan = require('morgan');
 var path = require('path');
 
@@ -17,6 +20,7 @@ app.get('/', function(req, res){
     res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
+
 app.listen(3000, function(){
-    debug('Listening on port ' + chalk.green('3000'));
+    debug(`listening on port ${chalk.green('3000')}`);
 });
